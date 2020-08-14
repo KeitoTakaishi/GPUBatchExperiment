7af3ee02adbebd6fa0c524a1a7a683d2388c26ad
KeitoTakaishi <takaishi78@gmail.com>
2020-08-14 22:10:29 +0900

    update

---
+++ Assets/Instancing_Frag/Scripts/instancing_frag.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instancing_frag : MonoBehaviour
+{
+    public int instanceCount = 10000;
+    public Mesh instanceMesh;
+    public Material instanceMaterial;
+    public int subMeshIndex = 0;
+
+    private int cachedInstanceCount = -1;
+    private int cachedSubMeshIndex = -1;
+    private ComputeBuffer positionBuffer;
+    private ComputeBuffer argsBuffer;
+    private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+
+    void Start()
+    {
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        UpdateBuffers();
+    }
+
+    void Update()
+    {
+        // Render
+        Graphics.DrawMeshInstancedIndirect(instanceMesh, subMeshIndex, instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), argsBuffer);
+    }
+
+    void UpdateBuffers()
+    {
+        // Ensure submesh index is in range
+        if(instanceMesh != null)
+            subMeshIndex = Mathf.Clamp(subMeshIndex, 0, instanceMesh.subMeshCount - 1);
+
+        // Positions
+        if(positionBuffer != null)
+            positionBuffer.Release();
+        positionBuffer = new ComputeBuffer(instanceCount, 16);
+        Vector4[] positions = new Vector4[instanceCount];
+        for(int i = 0; i < instanceCount; i++)
+        {
+            var p = Random.insideUnitSphere * 10.0f;
+            positions[i] = new Vector4(p.x, p.y, p.z, 0.1f);
+        }
+        positionBuffer.SetData(positions);
+        instanceMaterial.SetBuffer("positionBuffer", positionBuffer);
+
+        // Indirect args
+        if(instanceMesh != null)
+        {
+            args[0] = (uint)instanceMesh.GetIndexCount(subMeshIndex);
+            args[1] = (uint)instanceCount;
+            args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
+            args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
+        } else
+        {
+            args[0] = args[1] = args[2] = args[3] = 0;
+        }
+        argsBuffer.SetData(args);
+
+        cachedInstanceCount = instanceCount;
+        cachedSubMeshIndex = subMeshIndex;
+    }
+
+    void OnDisable()
+    {
+        if(positionBuffer != null)
+            positionBuffer.Release();
+        positionBuffer = null;
+
+        if(argsBuffer != null)
+            argsBuffer.Release();
+        argsBuffer = null;
+    }
+}
---
+++ Assets/Instancing_Frag/Shaders/instancing_frag.shader
@@ -0,0 +1,52 @@
+﻿Shader "Unlit/instancing_frag"
+{
+    Properties
+    {
+		_Color("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+    }
+    SubShader
+    {
+        Pass
+        {
+			CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma multi_cimpile_instancing
+			#pragma target 4.5
+			#include "unityCG.cginc"
+			
+
+#if SHADER_TARGET >= 45
+	StructuredBuffer<float4> positionBuffer;
+#endif
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+
+			half4 _Color;
+
+			v2f vert(appdata_full v, uint instanceID : SV_InstanceID)
+			{
+#if SHADER_TARGET >= 45
+				float4 data = positionBuffer[instanceID];
+#else
+				float4 data = 0;
+#endif
+				v2f o;
+				float3 localPosition = v.vertex.xyz * data.w;
+				float3 worldPosition = data.xyz + localPosition;
+				o.vertex = mul(UNITY_MATRIX_VP, float4(worldPosition, 1.0f));
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				return _Color;
+			}
+            ENDCG
+        }
+    }
+}
